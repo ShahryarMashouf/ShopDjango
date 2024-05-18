09e291cbfbdcc7ff83aa1a181bd47750f3a070b3
Shahryar <shahryarmashouf.1998@gmail.com>
2024-05-18 22:54:24 +0430

    Shop_django

---
+++ products/admin.py
@@ -0,0 +1,12 @@
+from . models import Product
+from django.contrib import admin
+from . models import Offer
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display =  ('name','price','stock')
+    
+class OfferAdmin(admin.ModelAdmin):
+    list_display =  ('code','discount')
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Offer,OfferAdmin)
